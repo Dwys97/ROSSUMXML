3cd9c779f3620e33b991e10a5f5023fc520b0385
Dwys97 <144741901+Dwys97@users.noreply.github.com>
2025-09-29 14:21:31 +0000

    Adjusted the API path

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-const API_BASE_URL = "http://localhost:3000"; // Default for local
+const API_BASE_URL = "https://animated-space-broccoli-7v945jv9p974hxgpx-3000.app.github.dev/"; // Default for local
