64cc174bd9d30daace90a8b9d6aa9e7d728c397e
Dwys97 <144741901+Dwys97@users.noreply.github.com>
2025-09-29 16:33:39 +0000

    Changed the API path to hardcoded PATH to Localhost instead of Variable, ensure to change before deploying to AWS Prod

--- frontend/script.js
+++ frontend/script.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             // Call raw XML endpoint
-            const response = await fetch(`${API_BASE_URL}api/transform`, {
+            const response = await fetch("http://localhost:3000/", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
