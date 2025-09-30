70771a2dae84ee0cb022bb6ea7992d1df1d94b15
Dwys97 <144741901+Dwys97@users.noreply.github.com>
2025-09-30 15:02:29 +0000

    added transformer page placeholder

---
+++ frontend/src/pages/TransformerPage.jsx
@@ -0,0 +1,13 @@
+// In frontend/src/pages/TransformerPage.jsx
+
+import React from 'react';
+
+export default function TransformerPage() {
+  // We will build the transformer UI here in the next step
+  return (
+    <div>
+      <h1>XML Transformer Tool</h1>
+      <p>This is where the transformer component will go.</p>
+    </div>
+  );
+}
