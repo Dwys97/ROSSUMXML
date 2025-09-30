82144e988f962a5c4ce16c69ab0a0a8012a6e968
Dwys97 <144741901+Dwys97@users.noreply.github.com>
2025-09-30 14:31:09 +0000

    changed python server fiile to treat jsx as js files

--- proxy_server.py
+++ proxy_server.py
@@ -8,8 +8,17 @@ BACKEND_URL = 'http://localhost:3000'
 DIRECTORY = 'frontend'
 
 class ProxyHandler(http.server.SimpleHTTPRequestHandler):
+    # --- START: Add this section to fix MIME types ---
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    # Add .jsx to the list of known script types
+    extensions_map = {
+        **http.server.SimpleHTTPRequestHandler.extensions_map,
+        '.jsx': 'text/javascript',
+    }
+    # --- END: Add this section ---
+
 
     def do_POST(self):
         if self.path.startswith('/api'):
