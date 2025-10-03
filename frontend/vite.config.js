bab8ac4b3d0c0d5882bfeda6511bdf8989f07b5b
Dwys97 <144741901+Dwys97@users.noreply.github.com>
2025-10-03 10:33:45 +0000

    registration and authorisation is now fully working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // If your backend routes don't start with /api
+        //rewrite: (path) => path.replace(/^\/api/, ''), // If your backend routes don't start with /api
       },
     },
   },
