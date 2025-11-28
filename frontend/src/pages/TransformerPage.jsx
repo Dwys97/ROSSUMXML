8e1420635402369e1a148c0ae38ebcf95a60c48c
R33kt <joelgra95@gmail.comm>
2025-11-28 17:44:48 +0000

    New Sidebar

--- frontend/src/pages/TransformerPage.jsx
+++ frontend/src/pages/TransformerPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import FileDropzone from "../components/common/FileDropzone";
 import Footer from "../components/common/Footer";
-import TopNav from "../components/TopNav";
+import SideNav from "../components/SideNav";
 import TransformationLimitModal from "../components/TransformationLimitModal";
 import { useAuth } from "../contexts/useAuth";
 import { tokenStorage } from "../utils/tokenStorage";
@@ -134,7 +134,7 @@ function TransformerPage() {
 
 	return (
 		<>
-			<TopNav />
+			<SideNav />
 
 			{/* Transformation Limit Modal */}
 			<TransformationLimitModal
@@ -204,30 +204,25 @@ function TransformerPage() {
 						</FileDropzone>
 					</div>
 
-					<FileDropzone onFileSelect={(files) => setDestinationXml(files[0])}>
-						<div className="icon">
-							<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<div className="bg-gray-100 border-2 border-gray-200 p-4 rounded-2xl w-fit">
+						<FileDropzone onFileSelect={(files) => setDestinationXml(files[0])}>
+							<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path
-									d="M14 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V8L14 2Z"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
+									d="M20.5 10.19H17.61C15.24 10.19 13.31 8.26 13.31 5.89V3C13.31 2.45 12.86 2 12.31 2H8.07C4.99 2 2.5 4 2.5 7.57V16.43C2.5 20 4.99 22 8.07 22H15.93C19.01 22 21.5 20 21.5 16.43V11.19C21.5 10.64 21.05 10.19 20.5 10.19ZM11.53 13.53C11.38 13.68 11.19 13.75 11 13.75C10.81 13.75 10.62 13.68 10.47 13.53L9.75 12.81V17C9.75 17.41 9.41 17.75 9 17.75C8.59 17.75 8.25 17.41 8.25 17V12.81L7.53 13.53C7.24 13.82 6.76 13.82 6.47 13.53C6.18 13.24 6.18 12.76 6.47 12.47L8.47 10.47C8.54 10.41 8.61 10.36 8.69 10.32C8.71 10.31 8.74 10.3 8.76 10.29C8.82 10.27 8.88 10.26 8.95 10.25C8.98 10.25 9 10.25 9.03 10.25C9.11 10.25 9.19 10.27 9.27 10.3C9.28 10.3 9.28 10.3 9.29 10.3C9.37 10.33 9.45 10.39 9.51 10.45C9.52 10.46 9.53 10.46 9.53 10.47L11.53 12.47C11.82 12.76 11.82 13.24 11.53 13.53Z"
+									fill="#64748B"
 								/>
-								<path d="M14 2V8H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-								<path d="M12 11V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
 								<path
-									d="M9 14L12 17L15 14"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
+									d="M17.43 8.80999C18.38 8.81999 19.7 8.81999 20.83 8.81999C21.4 8.81999 21.7 8.14999 21.3 7.74999C19.86 6.29999 17.28 3.68999 15.8 2.20999C15.39 1.79999 14.68 2.07999 14.68 2.64999V6.13999C14.68 7.59999 15.92 8.80999 17.43 8.80999Z"
+									fill="#64748B"
 								/>
 							</svg>
-						</div>
-						<h3>Destination Template</h3>
-						<p>Drop a single XML template</p>
-					</FileDropzone>
+							Destination Template
+							<h3 className="text-xl">Drop a single XML Template</h3>
+							<span className="file-count">
+								Selected: <span id="sourceCount">{sourceCount}</span>
+							</span>
+						</FileDropzone>
+					</div>
 
 					<FileDropzone onFileSelect={(files) => setXsdSchema(files[0])}>
 						<div className="icon">
